b3080bb7c3f72a5b4f6faa4b43f043551181a13f
Nick Pattison <pattison.nick@gmail.com>
2022-11-30 19:58:12 -0500

    Prevent player tilting; refactor PlayerController

--- Assets/Prefabs/Player/PlayerController.cs
+++ Assets/Prefabs/Player/PlayerController.cs
@@ -33,56 +33,58 @@ public class PlayerController : MonoBehaviour {
         pawn = GetComponent<CharacterController>();
         animator = GetComponentInChildren<Animator>();
     }
-
-    void Update() {
-
-        
-
-        bool isJumpHeld = Input.GetButton("Jump");
-        bool onJumpPress = Input.GetButtonDown("Jump");
-
-        // what should W do?
-        // if looking from above, should move the player up the screen (along the ground)
-        // if looking from side, should move player into the screen (along the ground)
+    void UpdateHorizontalInput(){
 
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         
-
+        // if looking from above, should move the player up the screen (along the ground)
+        // if looking from side, should move player into the screen (along the ground)
         float alignedHorizontal = Mathf.Abs(Vector3.Dot(cam.transform.up, Vector3.up));
         // 1 camera is horizontal
         // 0 camera is vertical
         // -1 camera is upside down ??
         Vector3 forward = Vector3.Lerp(cam.transform.up, cam.transform.forward, alignedHorizontal);
         inputDirection = forward * v + cam.transform.right * h;
-
+        // the above input should be close to lateral (assuming no camera-roll)
+        // but we can force-flatten it here
+        inputDirection.y = 0;
+        // if this changes, the player's rotation can get messed up
+        // normalize the input
         if (inputDirection.sqrMagnitude > 1) inputDirection.Normalize();
-
-        // if TRYING TO MOVE
-        if (inputDirection.sqrMagnitude > .25f) {
-
-            // turn to face the correct direction
-            transform.rotation = AnimMath.Slide(transform.rotation, Quaternion.LookRotation(inputDirection, Vector3.up), .01f);
+    }
+    void UpdateVerticalVelocity(){
+        bool isJumpHeld = Input.GetButton("Jump");
+        bool onJumpPress = Input.GetButtonDown("Jump");
+        if (isGrounded && onJumpPress) {
+            verticalVelocity = -jumpImpulse;
+            timeLeftGrounded = 0; // not on ground (for animation's sake)
+            // beginning a jump!
+        } else {
+            // apply gravity
+            verticalVelocity += gravityMultiplier * Time.deltaTime;
         }
+    }
+    void Update() {
 
-        // apply gravity
-        verticalVelocity += gravityMultiplier * Time.deltaTime;
-
-        // add lateral movement to vertical movement
-        Vector3 moveDelta = inputDirection * walkSpeed + verticalVelocity * Vector3.down;
+        UpdateHorizontalInput();
+        UpdateVerticalVelocity();
 
         // move pawn
+        Vector3 moveDelta = inputDirection * walkSpeed + verticalVelocity * Vector3.down;
         CollisionFlags flags = pawn.Move(moveDelta * Time.deltaTime);
         if (pawn.isGrounded) {
-            verticalVelocity = 0; // on ground, zero-out vertical-velocity
+            verticalVelocity = 0;
             timeLeftGrounded = .2f;
         }
 
-        if(isGrounded) {
-            if (isJumpHeld) {
-                verticalVelocity = -jumpImpulse;
-                timeLeftGrounded = 0; // not on ground (for animation's sake)
-            }
+        UpdateAnimations();
+    }
+    void UpdateAnimations(){
+        // if TRYING TO MOVE
+        if (inputDirection.sqrMagnitude > .25f) {
+            // turn to face the correct direction
+            transform.rotation = AnimMath.Slide(transform.rotation, Quaternion.LookRotation(inputDirection, Vector3.up), .01f);
         }
         animator?.SetBool("in air", !isGrounded);
         animator?.SetFloat("vertical speed", Mathf.Clamp(verticalVelocity / -8, -1, 1));
